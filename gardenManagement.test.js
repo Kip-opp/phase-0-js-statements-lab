e7c1316a8bd36cb7e3029a52f8121a8417480fae
denis@kyp <kypvega@gmail.com>
2025-11-18 14:13:06 +0300

    Complete: all tests passing

--- gardenManagement.test.js
+++ gardenManagement.test.js
@@ -11,17 +11,17 @@ describe('Garden Management Tasks', () => {
   });
 
   test('should print "Watering on" if temperature is greater than 80', () => {
-    require('../gardenManagement'); // Requiring the file should execute the code
+    require('./gardenManagement'); // Requiring the file should execute the code
     expect(consoleSpy).toHaveBeenCalledWith('Watering on');
   });
 
   test('should print "Lights off" if timeOfDay is morning', () => {
-    require('../gardenManagement'); // Requiring the file should execute the code
+    require('./gardenManagement'); // Requiring the file should execute the code
     expect(consoleSpy).toHaveBeenCalledWith('Lights off');
   });
 
   test('should increment soilMoisture to 40 using a while loop', () => {
-    require('../gardenManagement'); // Requiring the file should execute the code
+    require('./gardenManagement'); // Requiring the file should execute the code
     expect(consoleSpy).toHaveBeenCalledWith(40); // Check that the final value of soilMoisture is 40
   });
 });
